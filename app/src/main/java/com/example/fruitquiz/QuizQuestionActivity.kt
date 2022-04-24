78b0cc15d5771618c3291155bea90d0d541592cc
harsh-2109 <harshdpandya1995@gmail.com>
2022-04-23 22:39:46 -0400

    option selection implementation has been completed

--- app/src/main/java/com/example/fruitquiz/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/fruitquiz/QuizQuestionActivity.kt
@@ -1,13 +1,14 @@
 package com.example.fruitquiz
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.AppCompatDrawableManager
 
-class QuizQuestionActivity : AppCompatActivity() {
+class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var tvQuestion: TextView
     private lateinit var ivFruitImage: ImageView
@@ -20,6 +21,7 @@ class QuizQuestionActivity : AppCompatActivity() {
     private lateinit var btnSubmit: Button
 
     private var totalQuestionCounts = Constant.getQuestions().size
+    private var mSelectedOption = 0
     private var currentQuestionCount = 1
     private var correctAnswersCount = 0
 
@@ -33,6 +35,16 @@ class QuizQuestionActivity : AppCompatActivity() {
         // To load initial data
         loadData()
 
+        clickEvent()
+
+    }
+
+    private fun clickEvent() {
+        tvOptionOne.setOnClickListener(this)
+        tvOptionTwo.setOnClickListener(this)
+        tvOptionThree.setOnClickListener(this)
+        tvOptionFour.setOnClickListener(this)
+        btnSubmit.setOnClickListener(this)
     }
 
     private fun loadData() {
@@ -59,5 +71,44 @@ class QuizQuestionActivity : AppCompatActivity() {
         tvOptionThree = findViewById(R.id.tv_option_three)
         tvOptionFour = findViewById(R.id.tv_option_four)
         btnSubmit = findViewById(R.id.btn_submit)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.tv_option_one -> selectedOptionItem(tvOptionOne,1)
+            R.id.tv_option_two -> selectedOptionItem(tvOptionTwo,2)
+            R.id.tv_option_three -> selectedOptionItem(tvOptionThree,3)
+            R.id.tv_option_four -> selectedOptionItem(tvOptionFour,4)
+            R.id.btn_submit -> {
+                Toast.makeText(this,"Needs to implement",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun selectedOptionItem(view: TextView, selectedOption: Int) {
+        mSelectedOption = selectedOption
+
+        defaultOptionViewStyle()
+
+        view.background = AppCompatResources.getDrawable(
+            this,R.drawable.selected_option_border_bg
+        )
+        view.setTypeface(view.typeface,Typeface.BOLD)
+    }
+
+    private fun defaultOptionViewStyle() {
+        val options = ArrayList<TextView>()
+
+        options.add(tvOptionOne)
+        options.add(tvOptionTwo)
+        options.add(tvOptionThree)
+        options.add(tvOptionFour)
+
+        for(option in options){
+            option.background = AppCompatResources.getDrawable(
+                this,R.drawable.default_option_border_bg
+            )
+            option.typeface = Typeface.DEFAULT
+        }
     }
 }
